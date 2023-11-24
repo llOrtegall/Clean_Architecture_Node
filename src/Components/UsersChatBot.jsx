2c0739c1b2f94b6a0c3cdbd13358e9d04f2484a1
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-11-24 16:55:02 -0500

    seguir con la logica del App

--- src/Components/UsersChatBot.jsx
+++ src/Components/UsersChatBot.jsx
@@ -33,40 +33,43 @@ export function UserChatBot () {
   }
 
   return (
+    <>
+    <h1 className='mx-2 text-center bg-blue-500 text-white font-bold py-2 rounded-lg'>Usuarios Registrados Por Chat Boot</h1>
     <table className='text-center mx-2'>
-    <tr>
-      <th>N°</th>
-      <th>Nombre</th>
-      <th>Cedula</th>
-      <th>Correo</th>
-      <th>Telefono</th>
-      <th>Fecha de Registro</th>
-      <th>Estado</th>
-      <th>Opc Usuario</th>
-    </tr>
-      { usuarios.map((usuario, index) => {
-        return (
-        <tr key={index}>
-          <td>{index + 1}</td>
-          <td>{usuario.nombre}</td>
-          <td>{usuario.cedula}</td>
-          <td>{usuario.correo}</td>
-          <td>{usuario.telefono}</td>
-          <td>{usuario.fregistro.split('T')[0]}</td>
-          {
-            usuario.Estado === 'Si Existe'
-              ? <td className='bg-green-400'>Registrado</td>
-              : <td className='bg-red-400'>No Registrado</td>
-          }
-          {
-            usuario.Estado === 'Si Existe'
-              ? <td className='bg-green-400'>User Ok</td>
-              : <td className='bg-yellow-400'>Opc User</td>
-          }
-        </tr>
-        )
-      })
-      }
+      <tr>
+        <th>N°</th>
+        <th>Nombre</th>
+        <th>Cedula</th>
+        <th>Correo</th>
+        <th>Telefono</th>
+        <th>Fecha de Registro</th>
+        <th>Estado</th>
+        <th>Opc Usuario</th>
+      </tr>
+        { usuarios.map((usuario, index) => {
+          return (
+          <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{usuario.nombre}</td>
+            <td>{usuario.cedula}</td>
+            <td>{usuario.correo}</td>
+            <td>{usuario.telefono}</td>
+            <td>{usuario.fregistro.split('T')[0]}</td>
+            {
+              usuario.Estado === 'Si Existe'
+                ? <td className='bg-green-400'>Registrado</td>
+                : <td className='bg-red-400'>No Registrado</td>
+            }
+            {
+              usuario.Estado === 'Si Existe'
+                ? <td className='bg-green-400'>User Ok</td>
+                : <td className='bg-yellow-400'>Opc User</td>
+            }
+          </tr>
+          )
+        })
+        }
     </table>
+    </>
   )
 }
