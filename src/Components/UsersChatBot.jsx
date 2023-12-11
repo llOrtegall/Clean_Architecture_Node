1f4da7f3239d370c95878f73b91f29a47b87f147
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-12-11 13:10:52 -0500

    ok

--- src/Components/UsersChatBot.jsx
+++ src/Components/UsersChatBot.jsx
@@ -2,7 +2,6 @@ import { UserContext } from '../context/UserContext.jsx'
 import { useState, useContext, useEffect } from 'react'
 import { RenderUsers } from './RenderUsers.jsx'
 import { Loading } from './IconSvg.jsx'
-import { useAuth } from '../Auth/AuthContext.jsx'
 
 function useFilters ({ usuarios }) {
   const [filterUsers, setFilterUsers] = useState('Ninguno')
@@ -19,23 +18,9 @@ export function UserChatBot () {
   const [usuarios, setUsuarios] = useState([])
   const { userfiltrados, setFilterUsers } = useFilters({ usuarios })
   const [loading, setLoading] = useState(true)
-  const { user } = useAuth()
   const { signalUser } = useContext(UserContext)
 
-  const { empresa } = user
-
-  const handleSelect = async (ev) => {
-    const { value } = ev.target
-    if (value === 'Multired') {
-      await getDataEmpresaMultired()
-    } else if (value === 'Servired') {
-      await getDataEmpresaServired()
-    } else if (value === 'Multired y Servired') {
-      setLoading(false)
-    }
-  }
-
-  const getDataEmpresaMultired = async () => {
+  const getDataUsers = async () => {
     setLoading(true)
     try {
       const response = await fetch('http://localhost:6060/clientes')
@@ -66,46 +51,9 @@ export function UserChatBot () {
     }
   }
 
-  const getDataEmpresaServired = async () => {
-    setLoading(true)
-    try {
-      const response = await fetch('http://localhost:6060/clientesServired')
-      if (response.status === 200) {
-        const data = await response.json()
-        const cedulas = data.map(user => user.cedula)
-
-        const response2 = await fetch('http://localhost:6060/getCF', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ ccs: cedulas })
-        })
-
-        if (response2.status === 200) {
-          const data2 = await response2.json()
-          const updatedUsers = data.map((user, index) => ({
-            ...user,
-            ...data2[index]
-          }))
-          setUsuarios(updatedUsers)
-          setLoading(false)
-        }
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
   useEffect(() => {
-    if (empresa === 'Multired y Servired') {
-      setLoading(false)
-    } else if (empresa === 'Multired') {
-      getDataEmpresaMultired()
-    } else if (empresa === 'Servired') {
-      getDataEmpresaServired()
-    }
-  }, [])
+    getDataUsers()
+  }, [signalUser])
 
   const handleFilter = (ev) => {
     setFilterUsers(ev.target.value)
@@ -119,15 +67,6 @@ export function UserChatBot () {
     <>
       <h1 className='text-center bg-blue-500 text-white font-bold py-2 rounded-lg flex items-center justify-center'>
         Usuarios Registrados Por Chat Boot
-        {
-          empresa === 'Multired y Servired'
-            ? <select className='ml-6 text-black' onChange={handleSelect}>
-                <option >Seleccione Empresa</option>
-                <option value='Servired'>Servired</option>
-                <option value='Multired'>Multired</option>
-              </select>
-            : null
-        }
       </h1>
       <table className='text-center'>
         <tr>
