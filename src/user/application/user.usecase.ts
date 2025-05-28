4e4bb4fdb6cce2c175a6ad1014eb8e7f87d4e81c
llOrtegall <IvanOrtega_97@hotmail.com>
2025-05-28 09:35:20 -0500

    refactor names on user use case

--- src/user/application/user.usecase.ts
+++ src/user/application/user.usecase.ts
@@ -2,28 +2,25 @@ import type { UserRepository } from "@domain/user.repository";
 import { type UserEntity } from '@domain/user.entity';
 import { UserValue } from "@domain/user.value";
 
+// UserUseCase CONSUME la interfaz (no la implementa)
 export class UserUseCase {
   constructor(private readonly userRepository: UserRepository) { }
+  // ↑ Recibe una INSTANCIA del repository (inyección de dependencias)
 
-  public registerUser = async ({ name, email, description, birthDate, document, telefono }: Omit<UserEntity, 'uuid'>) => {
-
+  public createNewUser = async ({ name, email, description, birthDate, document, telefono }: Omit<UserEntity, 'uuid'>) => {
     const userValue = new UserValue({ name, email, description, birthDate, document, telefono });
-
     const userCreated = await this.userRepository.registerUser(userValue);
-
     return userCreated;
   }
 
-  public getDetailUser = async (uuid: string) => {
-
-    const user = await this.userRepository.findUserById(uuid)
-    
-    return user
+  public getUserProfile = async (uuid: string) => {
+    const user = await this.userRepository.findUserById(uuid);
+    return user;
   }
 
-  public listUsers = async () => {
-    const users = await this.userRepository.listUsers()
-    return users
+  public getAllUsers = async () => {
+    const users = await this.userRepository.listUsers();
+    return users;
   }
 
 }
