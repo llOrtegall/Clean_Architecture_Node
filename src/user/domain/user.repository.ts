1e350d51ed5e3d666f386637bec5c4b0ca9a48a6
llOrtegall <IvanOrtega_97@hotmail.com>
2025-06-17 15:10:47 -0500

    delete files

--- src/user/domain/user.repository.ts
+++
@@ -1,7 +0,0 @@
-import type { UserEntity } from "@domain/user.entity";
-
-export interface UserRepository {
-  registerUser(user: UserEntity): Promise<UserEntity | null>;
-  findUserById(uuid: string): Promise<UserEntity | null>;
-  listUsers(): Promise<UserEntity[] | null>
-}
