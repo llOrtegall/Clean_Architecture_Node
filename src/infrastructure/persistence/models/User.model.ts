2ea6fb3aeef4e4f0d4d79b82ad71b8451fc45401
llOrtegall <IvanOrtega_97@hotmail.com>
2025-06-19 11:10:29 -0500

    add UserModel class for user persistence with Sequelize

---
+++ src/infrastructure/persistence/models/User.model.ts
@@ -0,0 +1,27 @@
+import { DataTypes, Model, type InferAttributes, type InferCreationAttributes, type CreationOptional } from 'sequelize';
+import { mysqlConn } from '@infrastructure/persistence/connection';
+import { User } from '@/domain/entities/User';
+
+class UserModel extends Model<InferAttributes<UserModel>, InferCreationAttributes<UserModel>> implements User {
+    declare id: CreationOptional<string>;
+    declare name: string;
+    declare email: string;
+    declare documentId: string;
+    declare password: string;
+    declare createdAt: CreationOptional<Date>;
+    declare updatedAt: CreationOptional<Date>;
+}
+
+UserModel.init({
+    id: { type: DataTypes.CHAR(36), primaryKey: true, allowNull: false },
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    documentId: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+}, {
+    sequelize: mysqlConn,
+    tableName: 'USERS',
+    timestamps: true
+})
