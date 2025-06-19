01afd7b718cd11b49cf7231ae3157e68a17b71fc
llOrtegall <IvanOrtega_97@hotmail.com>
2025-06-19 11:10:24 -0500

    add MySQL connection setup using Sequelize

---
+++ src/infrastructure/persistence/connection/index.ts
@@ -0,0 +1,13 @@
+import { MYSQL_DATABASE, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_PORT, MYSQL_USER, } from '@infrastructure/configuration/mysqSchema';
+import { Sequelize } from 'sequelize';
+
+export const mysqlConn = new Sequelize(
+    MYSQL_DATABASE,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+    {
+        host: MYSQL_HOST,
+        port: MYSQL_PORT,
+        dialect: 'mysql'
+    }
+);
